28192b30537d964b34fb53c30844e575fa9637ea
Akhancode <akhan.code@gmail.com>
2023-01-31 09:53:01 +0530

    applied prettify in Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,16 +1,16 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
-const Context = ({children}) => {
-const [authorised,setAuthorised] = useState(false)
-  const [user,setUser] = useState(null)
+const Context = ({ children }) => {
+  const [authorised, setAuthorised] = useState(false);
+  const [user, setUser] = useState(null);
 
   return (
-    <UserContext.Provider value={{authorised,setAuthorised,user,setUser}}>
-        {children}
+    <UserContext.Provider value={{ authorised, setAuthorised, user, setUser }}>
+      {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
-export default Context
+export default Context;
